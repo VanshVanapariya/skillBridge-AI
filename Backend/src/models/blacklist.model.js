df75535130f4eae7504c5cf691cd4e5e1c309c8b
Vansh Vanapariya <vanshvanapariya268@gmail.com>
2026-04-13 14:12:21 +0530

    Complete Authentication

---
+++ Backend/src/models/blacklist.model.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose")
+
+
+const blacklistSchema = new mongoose.Schema({
+    token: {
+        type: String,
+        required: [true, "token is required to be added in blacklist"]
+    }
+}, {
+    timestamps: true
+})
+
+const tokenBlacklistModel = mongoose.model("Blacklist", blacklistSchema)
+
+module.exports = tokenBlacklistModel
+
