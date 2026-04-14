e354ef8097fdb3da9f27173b371388e88e5db069
Vansh Vanapariya <vanshvanapariya268@gmail.com>
2026-04-14 14:37:27 +0530

    Add routing and authentication pages

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const cookieParser = require('cookie-parser')
+const cors = require('cors')
 
 const app = express()
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:3001",
+    credentials: true
+}))
 
 /* require all the routes here */
 const authRouter = require("./routs/auth.routs")
--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,13 @@
-
+import { RouterProvider } from "react-router-dom";
+import { router } from "./app.routs";
+import { AuthProvider } from "./features/auth/auth.context";
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>
   )
 }
 
